466ed22908785bcdaf0a68578cfa2f4ffd395d69
Sarah Llewelyn <se.llewelyn@gmail.com>
2020-01-14 23:44:34 -0500

    begin regex

--- src/playground/main.go
+++ src/playground/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -11,10 +12,12 @@ func main () {
 	fmt.Println("Welcome to go tic tac toe.  There's no turning back now...")
 
 	reader := bufio.NewReader(os.Stdin)
+	pattern := regexp.MustCompile(`(?P<x>[012]) (?P<y>[012])`)
 	for {
 		fmt.Println("Take your next move (X, Y)")
 
 		text, _ := reader.ReadString('\n')
+		pattern.FindStringSubmatch(text)
 		fmt.Println("Got it,", text)
 
 		if strings.TrimSpace(text) == "quit" {
